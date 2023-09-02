65ef8e7d55dd24471870144166a9dc9d57f00f61
Pooja Indulkar <98650416+poojaindulkar@users.noreply.github.com>
2023-09-03 01:41:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("Today.ejs", {
+    res.render("today.ejs", {
         List: toDoListToday,
         
     });

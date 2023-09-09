2eeca2b0f1ec095f8da71e8fec2158160ad7bcea
Pooja Indulkar <98650416+poojaindulkar@users.noreply.github.com>
2023-09-09 20:03:32 +0530

    Database Connection

--- index.js
+++ index.js
@@ -1,42 +1,80 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express();
 const port = 3000;
-let toDoListToday = [];
-let toDoListWork = [];
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.render("today.ejs", {
-        List: toDoListToday,
-        
-    });
+//Database connnection
+const mongo_URL=process.env.URL;
+mongoose.connect(mongo_URL);
+
+//new Schema
+const itemSchema = new mongoose.Schema({
+  task: String,
+});
+const Item = mongoose.model("item", itemSchema);
+const WorkList = mongoose.model("workList", itemSchema);
+
+
+app.get("/:route", (req, res) => {
+  const route = req.params.route;
+  if (route === "home") {
+    Item.find({})
+      .then(function (todayItems) {
+        res.render("today.ejs", {
+          List: todayItems,
+        });
+      })
+  } else {
+    WorkList.find({})
+      .then(function (WorkItems) {
+        res.render("work.ejs", {
+          List: WorkItems,
+        });
+      })
+  }
 });
 
-app.get("/work", (req, res) => {
-    res.render("work.ejs", {
-        List: toDoListWork,
-        
-    });
-});
-
-app.post("/", (req, res) => {
-    const newItem = req.body["ListItem"];
-    toDoListToday.push(newItem);
-    res.redirect("/");
-});
-
-app.post("/work", (req, res) => {
-    const newItem = req.body["ListItem"];
-    toDoListWork.push(newItem);
-
+app.post("/:route", (req, res) => {
+  const route = req.params.route;
+  const newItem = req.body["ListItem"];
+  if (route === "home") {
+    Item.create({ task: newItem });
+    res.redirect("/home");
+  } else {
+    WorkList.create({ task: newItem });
     res.redirect("/work");
+  }
+});
+
+app.post("/delete/:route", (req, res) => {
+  const route = req.params.route;
+  const checkItemId = req.body.checkbox;
+
+  if (route === "home") {
+    Item.findByIdAndRemove(checkItemId)
+      .catch(function (err) {
+        console.log(err);
+      });
+    res.redirect("/home");
+  } else {
+    WorkList.findByIdAndRemove(checkItemId)
+      .then(function () {
+        console.log("Succsessfully removed item");
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+    res.redirect("/work");
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Listening to port ${port}`);
+  console.log(`Listening to port ${port}`);
 });
